d0322245316f7ed8a9bba04ac48307f85e3c29b9
GrafilIan <dragiancarl02@gmail.com>
2022-12-27 17:15:35 +0800

    updated

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "ef2a64077dff491166daa75d0673cd01"
+    }
+}
+
+module.exports = constants;
